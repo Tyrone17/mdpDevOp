c9d1f0bf5e2e20f039177db744309bbee0c0ad5f
tyrone17 <tyronemaasdorp@gmail.com>
2023-09-28 12:50:57 +0200

    Update App.js
    Updated the App component

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>
-          m d p<br></br>
-          <span>v27.09.23</span>
+          m d p
+          {/* <span>v27.09.23</span> */}
         </h1>
         {/* <h5>v22.09.23</h5> */}
         {/* <h5>v23.09.23 - stable version</h5> */}
         {/* <h6>v24.09.23 - nightly</h6> */}
+        <h6>v27.09.23</h6>
       </header>
     </div>
   );
